5d2d438ce01709e79e29944db02b48aed941ba0b
Christopher Otto <christopher.otto@nordakademie.de>
2021-06-04 15:46:50 +0000

    Fix CSS loading issue

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  });
+
+  return <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Component {...pageProps} />
+    </ThemeProvider>
 }
 
 export default MyApp
