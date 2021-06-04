44c4ac82bba7949e3248dfa8e4109e2f46e47a48
MarzellArwedWittmaack-A16 <marzell_arwed.wittmaack@nordakademie.de>
2021-06-04 07:03:40 +0000

    added material-ui to frontend

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,26 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import React from 'react';
+import theme from '../src/theme';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+
+  return (
+    <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
---
+++ frontend/pages/topic/create.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@material-ui/core";
+import React from "react";
+
+function create() {
+    return <Container>test</Container>;
+}
+
+export default create;
