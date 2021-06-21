9c8ec7a70121c925921e95d07c262b226f634bb4
Sebastian Schimpfhauser - A16 <sebastian.schimpfhauser@nordakademie.de>
2021-06-21 14:49:55 +0200

    Mail-Benachrichtigung bei neuem Thema

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
